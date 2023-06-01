564535a1d727513bbd5e60be613e5572aab5e601
Joy <85593549+yun9706@users.noreply.github.com>
2023-06-01 11:18:16 +0900

    Update tistory.py

--- tistory.py
+++ tistory.py
@@ -5,21 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
 import datetime as dt 
-# from selenium.webdriver.chrome.options import Options
-# from webdriver_manager.chrome import ChromeDriverManager
-# chrome_options = Options()
-# chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
-# service = Service(executable_path=ChromeDriverManager().install()) #크롬드라이버 매니저를 통해서 자동으로 크롬드라이버 최신버전 가져온 다음, 서비스 객체를 만들어서 서비스 변수에 저장한다.
-# driver = webdriver.Chrome(service = service, options=chrome_options) #크롬 옵션이 담긴 chrome_options 추가
 
-# csv_path = './2022-07-12밀양크롤링.csv'
-
-# csv_file = pd.read_csv(csv_path)
-# tistoryurls = csv_file['origin'] == 'tistory'
-# tistory_csv = csv_file[tistoryurls]
-# tistory = tistory_csv['url']
-#craw_text = tistory_csv['searchword'] 
-#print(csv_file.head())
 
 def crawl_tistory(url) :
     options=webdriver.ChromeOptions()
@@ -52,13 +38,9 @@ def crawl_tistory(url) :
     
     if text == "":
         text = soup.select_one('div.contents_style').text
-        text = re.sub(' +', ' ', re.sub(r'\xa0|\u200b|\n', '', text)).strip()# try:
-        #     text = soup.select_one('#content > div > div.entry-content > div.contents_style').text
-        #     text = re.sub(' +', ' ', re.sub(r'\xa0|\u200b|\n', '', text)).strip()
-        # except:
-        #     text = soup.select_one('#container > main > div > div.area-view > div.article-view > div.contents_style').text
-        #     text = re.sub(' +', ' ', re.sub(r'\xa0|\u200b|\n', '', text)).strip()
-
+        text = re.sub(' +', ' ', re.sub(r'\xa0|\u200b|\n', '', text)).strip()
+        
+        
     # 제목 스크래핑
     title = soup.select_one('a.current')
     
@@ -67,20 +49,3 @@ def crawl_tistory(url) :
 
     return text
 
-# for i in tistory[:3] :
-#     try:
-#         texts = crawl_tistory(i)
-#     except:
-#         pass
-#        #tistory_text.append('None')
-#         #df_1 = csv_file['url'] == i
-#     else:
-#         a = csv_file[csv_file['url'] == i].index
-#         csv_file.loc[a, 'text'] = texts
-#         #print(csv_file.loc[a])
-#     time.sleep(random.uniform(0.5, 1.5))
-    
-    #csv_file.to_csv('크롤링.csv', index=False, mode='a', encoding='utf-8')
-    
-#df =  pd.DataFrame({'url' : tistory_url, 'title' : tistory_title, 'text' : tistory_text, 'date_c' : collect_date, 'date_w' : tistory_date})   
-#df.to_csv('밀양_티스토리.csv', index=False)
